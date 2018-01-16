a1999859a5ab658e1208a36562b39b01281d3acc
sw9t <sw9t@ubuntu-Z510.Home>
2018-01-16 19:05:51 +0200

    sw9t

--- frontend/views/layouts/navbar.php
+++ frontend/views/layouts/navbar.php
@@ -22,112 +22,8 @@ use yii\helpers\Url;
         <div class="topmenu-outer">
             <div class="top-menu">
                 <ul class="nav navbar-nav navbar-left">
-                    <li>
-                        <a href="javascript:void(0);" class="waves-effect waves-button waves-classic sidebar-toggle"><i
-                                    class="fa fa-bars"></i></a>
-                    </li>
-                    <li>
-                        <a href="#cd-nav" class="waves-effect waves-button waves-classic cd-nav-trigger"><i
-                                    class="fa fa-diamond"></i></a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);" class="waves-effect waves-button waves-classic toggle-fullscreen"><i
-                                    class="fa fa-expand"></i></a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle waves-effect waves-button waves-classic"
-                           data-toggle="dropdown">
-                            <i class="fa fa-cogs"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-md dropdown-list theme-settings" role="menu">
-                            <li class="li-group">
-                                <ul class="list-unstyled">
-                                    <li class="no-link" role="presentation">
-                                        Fixed Header
-                                        <div class="ios-switch pull-right switch-md">
-                                            <input type="checkbox" class="js-switch pull-right fixed-header-check"
-                                                   checked>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="li-group">
-                                <ul class="list-unstyled">
-                                    <li class="no-link" role="presentation">
-                                        Fixed Sidebar
-                                        <div class="ios-switch pull-right switch-md">
-                                            <input type="checkbox" class="js-switch pull-right fixed-sidebar-check">
-                                        </div>
-                                    </li>
-                                    <li class="no-link" role="presentation">
-                                        Horizontal bar
-                                        <div class="ios-switch pull-right switch-md">
-                                            <input type="checkbox" class="js-switch pull-right horizontal-bar-check">
-                                        </div>
-                                    </li>
-                                    <li class="no-link" role="presentation">
-                                        Toggle Sidebar
-                                        <div class="ios-switch pull-right switch-md">
-                                            <input type="checkbox" class="js-switch pull-right toggle-sidebar-check">
-                                        </div>
-                                    </li>
-                                    <li class="no-link" role="presentation">
-                                        Compact Menu
-                                        <div class="ios-switch pull-right switch-md">
-                                            <input type="checkbox" class="js-switch pull-right compact-menu-check">
-                                        </div>
-                                    </li>
-                                    <li class="no-link" role="presentation">
-                                        Hover Menu
-                                        <div class="ios-switch pull-right switch-md">
-                                            <input type="checkbox" class="js-switch pull-right hover-menu-check">
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="li-group">
-                                <ul class="list-unstyled">
-                                    <li class="no-link" role="presentation">
-                                        Boxed Layout
-                                        <div class="ios-switch pull-right switch-md">
-                                            <input type="checkbox" class="js-switch pull-right boxed-layout-check"
-                                                   checked>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="li-group">
-                                <ul class="list-unstyled">
-                                    <li class="no-link" role="presentation">
-                                        Choose Theme Color
-                                        <div class="color-switcher">
-                                            <a class="colorbox color-blue" href="?theme=blue" title="Blue Theme"
-                                               data-css="blue"></a>
-                                            <a class="colorbox color-green" href="?theme=green" title="Green Theme"
-                                               data-css="green"></a>
-                                            <a class="colorbox color-red" href="?theme=red" title="Red Theme"
-                                               data-css="red"></a>
-                                            <a class="colorbox color-white" href="?theme=white" title="White Theme"
-                                               data-css="white"></a>
-                                            <a class="colorbox color-purple" href="?theme=purple" title="purple Theme"
-                                               data-css="purple"></a>
-                                            <a class="colorbox color-dark" href="?theme=dark" title="Dark Theme"
-                                               data-css="dark"></a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="no-link">
-                                <button class="btn btn-default reset-options">Reset Options</button>
-                            </li>
-                        </ul>
-                    </li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="javascript:void(0);" class="waves-effect waves-button waves-classic show-search"><i
-                                    class="fa fa-search"></i></a>
-                    </li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle waves-effect waves-button waves-classic"
                            data-toggle="dropdown"><i class="fa fa-envelope"></i><span
@@ -243,26 +139,14 @@ use yii\helpers\Url;
                             <img class="img-circle avatar" src="/images/avatar1.png" width="40" height="40" alt="">
                         </a>
                         <ul class="dropdown-menu dropdown-list" role="menu">
-                            <li role="presentation"><a href="profile.html"><i class="fa fa-user"></i>Profile</a></li>
-                            <li role="presentation"><a href="calendar.html"><i class="fa fa-calendar"></i>Calendar</a>
+                            <li role="presentation"><a href="profile.html"><i class="fa fa-user"></i>Профиль</a></li>
+                            <li role="presentation"><a href="<?= Url::to(['/site/logout']) ?>" data-method="post">
+                                    <i class="fa fa-sign-out m-r-xs"></i>
+                                    Выход</a>
                             </li>
-                            <li role="presentation"><a href="inbox.html"><i class="fa fa-envelope"></i>Inbox<span
-                                            class="badge badge-success pull-right">4</span></a></li>
-                            <li role="presentation" class="divider"></li>
-                            <li role="presentation"><a href="lock-screen.html"><i class="fa fa-lock"></i>Lock screen</a>
-                            </li>
-                            <li role="presentation"><a href="login.html"><i class="fa fa-sign-out m-r-xs"></i>Log
-                                    out</a></li>
                         </ul>
                     </li>
-                    <li>
-                        <a href="<?= Url::to(['/site/logout']) ?>" data-method="post"
-                           class="log-out waves-effect waves-button waves-classic">
-                            <span> <?= BaseHtml::icon('log-out') ?>
-                                <?= $user->isNewRecord? ' Войти' : ' Выход'?> </span>
-                        </a>
-                    </li>
-                    <li>
+                    <li class="hidden">
                         <a href="javascript:void(0);" class="waves-effect waves-button waves-classic" id="showRight">
                             <i class="fa fa-comments"></i>
                         </a>
--- frontend/views/layouts/sidebar.php
+++ frontend/views/layouts/sidebar.php
@@ -7,7 +7,7 @@ use yii\helpers\Url;
     <div class="page-sidebar-inner slimscroll">
         <div class="sidebar-header">
             <div class="sidebar-profile">
-                <a href="javascript:void(0);" id="profile-menu-link">
+                <a href="" id="profile-menu-link">
                     <div class="sidebar-profile-image">
                         <img src="/images/profile-menu-image.png" class="img-circle img-responsive" alt="">
                     </div>
@@ -19,16 +19,9 @@ use yii\helpers\Url;
             </div>
         </div>
         <ul class="menu accordion-menu">
-            <li class="droplink active"><a href="<?= Url::to(['/tickets/index']) ?>"
-                                           class="waves-effect waves-button">
-                    <span class="menu-icon glyphicon glyphicon-list-alt"></span>
-                    <p>Заявки</p><span class="arrow"></span></a>
-                <ul class="sub-menu">
-                    <li><a href="<?= Url::to(['/tickets/index']) ?>">Мои заявки</a></li>
-                    <li class="active"><a href="<?= Url::to(['/tickets/index']) ?>">В обработке</a></li>
-                    <li><a href="<?= Url::to(['/tickets/index']) ?>">Свободные заявки</a></li>
-                </ul>
-            </li>
+            <li class="<?=$_SERVER['REQUEST_URI']=='/tickets/index' ? 'active' : ''?>"><a href="<?= Url::to(['/tickets/index']) ?>">Мои заявки</a></li>
+            <li class="<?=$_SERVER['REQUEST_URI']=='/tickets/index' ? 'active' : ''?>"><a href="<?= Url::to(['/tickets/index']) ?>">В обработке</a></li>
+            <li class=""><a href="<?= Url::to(['/tickets/index']) ?>">Свободные заявки</a></li>
             <li class="droplink"><a href="#" class="waves-effect waves-button">
                     <span class="menu-icon glyphicon glyphicon-user"></span>
                     <p>Пользователи</p><span class="arrow"></span></a>
@@ -46,24 +39,6 @@ use yii\helpers\Url;
                 <ul class="sub-menu">
                     <li><a href="<?= Url::to(['/status-tickets']) ?>">Статусы заявок</a></li>
 
-                </ul>
-            </li>
-
-            <li class="droplink"><a href="#" class="waves-effect waves-button"><span
-                            class="menu-icon glyphicon glyphicon-flash"></span>
-                    <p>Levels</p><span class="arrow"></span></a>
-                <ul class="sub-menu">
-                    <li class="droplink"><a href="#"><p>Level 1.1</p><span class="arrow"></span></a>
-                        <ul class="sub-menu">
-                            <li class="droplink"><a href="#"><p>Level 2.1</p><span class="arrow"></span></a>
-                                <ul class="sub-menu">
-                                    <li><a href="#">Level 3.1</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Level 2.2</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Level 1.2</a></li>
                 </ul>
             </li>
         </ul>
